9b7f236c7b9fd2f6d5aac756e3f73eac1741253b
Albani <50041403+b4n88@users.noreply.github.com>
2024-12-27 09:40:26 +0800

    Delete Data_igfs/Penggunaan.py

--- Data_igfs/Penggunaan.py
+++
@@ -1,6 +0,0 @@
-input_file = 'data_input.csv'        # Ganti dengan path file input Anda
-output_file = 'data_output.csv'      # File dengan kolom 'Cabang' yang diperbarui
-output_tunai = 'dataset_tunai.csv'   # File untuk dataset TUNAI_AMOUNT
-output_type_trx = 'dataset_type_trx.csv'  # File untuk dataset TYPE_TRX
-
-proses_file_csv(input_file, output_file, output_tunai, output_type_trx)
